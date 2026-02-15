a4dd01f819356f6a83eca3becaaac672815f8d5a
olarex358 <ridwanullah24@gmail.com>
2026-02-14 20:10:53 -1200

    converting to react

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ const allowList = [
   // Optional if you run frontend on 3000
   "http://localhost:3000",
   "http://127.0.0.1:3000",
+   "http://localhost:5173",
+  "http://127.0.0.1:5173",
 ];
 
 const corsOptions = {
